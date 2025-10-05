e94e536c426f8c7e42e79312804929fee148f4b1
davidegc <davidegc@uci.cu>
2025-10-05 12:03:37 -0400

    Subiendo Portafolio

---
+++ src/data/profile.js
@@ -0,0 +1,99 @@
+
+
+export const profile = {
+    name: "David Emanuel Guilarte Correa ",
+    age: "24",
+    address: "Reforma Urbana Edificio 11 apto E",
+    email: "https://workspace.google.com/davidguilartecorrea@gmail.com",
+    phone: " +53 59501266 ",
+    about: "  Hola! Soy un Desarrollador Frontend apasionado por crear experiencias web atractivas, funcionales y accesibles. Con conocimientos en lenguajes de desarrollo web como HTML,CSS,JavaScript y experiencia en el manejo de Drupal como CMS, me especializo en transformar ideas en interfaces dinámicas y optimizadas para el usuario.",
+
+
+    experiencia: [
+        {
+            company: "Centro de Soluciones Libres y Desarrollo Web CISOL",
+            role: "Desarrollador Frontend",
+            period: "2025-Presente",
+            description: "Desarrollo de soluciones web y mantemiento de portales como ZED Mariel."
+        }
+        ,
+        {
+            company: "Centro de Soluciones Libres y Desarrollo Web CISOL",
+            role: "Desarrollador Frontend",
+            period: "2025-Presente",
+            description: "Desarrollo de soluciones web y mantemiento de portales como ZED Mariel."
+        }
+        ,
+        {
+            company: "Centro de Soluciones Libres y Desarrollo Web CISOL",
+            role: "Desarrollador Frontend",
+            period: "2025-Presente",
+            description: "Desarrollo de soluciones web y mantemiento de portales como ZED Mariel."
+        }
+        ,
+        {
+            company: "Centro de Soluciones Libres y Desarrollo Web CISOL",
+            role: "Desarrollador Frontend",
+            period: "2025-Presente",
+            description: "Desarrollo de soluciones web y mantemiento de portales como ZED Mariel."
+        }
+        ,
+    ]
+    ,
+    estudios: [{
+        degree: "Ingeniería en Ciencias Informáticas",
+        institucion: "Universidad de las Ciencias Informáticas UCI",
+        period: "2021-2024"
+
+    }
+        ,
+    {
+        degree: "Ingeniería en Ciencias Informáticas",
+        institucion: "Universidad de las Ciencias Informáticas UCI",
+        period: "2021-2024"
+
+    }
+    ],
+    habilidades: [{
+        tecnology: "Html",
+        level: "73",
+    },
+    {
+        tecnology: "CSS",
+        level: "83",
+    },
+    {
+        tecnology: "JavaScript",
+        level: "59"
+    },
+    {
+        tecnology: "Python",
+        level: "46"
+    },
+
+    {
+        tecnology: "SQL",
+        level: "75"
+    },
+
+
+    {
+        tecnology: "API REST",
+        level: "46"
+    },
+
+
+    ],
+    projects: [
+        {
+            name: "FavInk Tattoo Studio",
+            description: "Portal web desarrollado para el artista Favier Guilarte Correa",
+            tecnology: " CMS Drupal  versión 10.5.6",
+            date: "En curso",
+            link: "example.com"
+
+
+        }
+        ,
+    ]
+}
